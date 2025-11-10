c73580a12535f1faa6e0562c568a2e1cea1ce769
EnriquePDabird <eph1806@gmail.com>
2025-11-10 17:18:10 +0000

    Añadido método createfolders

--- src/main/java/network/ARGOHttpRequest.java
+++ src/main/java/network/ARGOHttpRequest.java
@@ -6,19 +6,58 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.nio.file.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class ARGOHttpRequest {
+    private final HttpClient defaultClient;
+    private final String urlAtlanticOcean2025 = "https://data-argo.ifremer.fr/geo/atlantic_ocean/2025/";
+    private final Path downloadDir = Paths.get("src/main/java/resources");
 
-    public HttpResponse<String> connectionRequestTester(){
-        HttpClient defaultClient = HttpClient.newBuilder()
-        .version(HttpClient.Version.HTTP_2)
-        .connectTimeout(Duration.ofSeconds(10))
-        .build();
-        
-        String urlAtlanticOcean2025 = "https://data-argo.ifremer.fr/geo/atlantic_ocean/2025/";
+    public ARGOHttpRequest(){
+        this.defaultClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+    }
 
+    // Crea las carpetas donde se almacenarán los archivos NetCDF
+    private void createFolders(){
+        String htmlBody = getHTMLContent(urlAtlanticOcean2025);
+        if(htmlBody == null){
+            System.err.println("No se pudo obtener el contenido de la URL base");
+        }
+
+        Document doc = Jsoup.parse(htmlBody, urlAtlanticOcean2025);
+        Elements monthLinks = doc.select("a[href~=^\\d{2}/$]");
+
+        System.out.println(monthLinks.size());
+        for(Element monthLink : monthLinks){
+            String monthHref = monthLink.attr("href");
+            String monthUrl = urlAtlanticOcean2025 + monthHref;
+
+            String monthDirName = monthHref.replace("/", "");
+
+            Path monthDir = this.downloadDir.resolve(monthDirName);
+            try {
+                Files.createDirectories(monthDir);
+                System.out.println("Carpeta creada (o ya existía): " + monthDir.toAbsolutePath());
+
+            } catch (IOException e) {
+                System.err.println("Error al crear la carpeta: " + monthDir.toAbsolutePath());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Devuelve el cuerpo de la petición HTML
+    private  String getHTMLContent(String url){
         HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(urlAtlanticOcean2025))
+        .uri(URI.create(url))
         .GET()
         .build();
 
@@ -26,10 +65,10 @@ public class ARGOHttpRequest {
             HttpResponse<String> response = defaultClient.send(request, HttpResponse.BodyHandlers.ofString());
             
             if(response.statusCode() >= 200 && response.statusCode() < 300){
-                return response;
+                return response.body();
             } else {
                 System.err.println("La solicitud falló con el código de estado: " + response.statusCode());
-                return response;
+                return null;
             }
         } catch (IOException | InterruptedException e) {
             // La solicitud no pudo completarse (timeout, no hay internet, error de DNS)
@@ -39,7 +78,8 @@ public class ARGOHttpRequest {
     }
 
     public static void main(String[] args){
-        ARGOHttpRequest tester = new ARGOHttpRequest();
-        System.out.println(tester.connectionRequestTester().body());
+        ARGOHttpRequest request = new ARGOHttpRequest();
+        request.createFolders();
+
     }
 }
